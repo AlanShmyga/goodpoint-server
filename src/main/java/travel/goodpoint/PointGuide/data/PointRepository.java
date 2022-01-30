1771733215fe6713e4c0587cbc1569a9a5a9bf2e
Alan Shmyha <alexey.shmyga@gmail.com>
2022-01-30 17:02:58 +0100

    Prohibiting Save and Delete operations in Point repository

--- src/main/java/travel/goodpoint/PointGuide/data/PointRepository.java
+++ src/main/java/travel/goodpoint/PointGuide/data/PointRepository.java
@@ -1,7 +1,43 @@
 package travel.goodpoint.PointGuide.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
 import travel.goodpoint.PointGuide.models.Point;
 
 public interface PointRepository extends CrudRepository<Point, Long> {
+
+    @Override
+    @NonNull
+    @RestResource(exported = false)
+    <S extends Point> S save(@NonNull S entity);
+
+    @Override
+    @NonNull
+    @RestResource(exported = false)
+    <S extends Point> Iterable<S> saveAll(@NonNull Iterable<S> entities);
+
+    @Override
+    @NonNull
+    @RestResource(exported = false)
+    void deleteById(@NonNull Long aLong);
+
+    @Override
+    @NonNull
+    @RestResource(exported = false)
+    void delete(@NonNull Point entity);
+
+    @Override
+    @NonNull
+    @RestResource(exported = false)
+    void deleteAllById(@NonNull Iterable<? extends Long> longs);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(@NonNull Iterable<? extends Point> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
