8fd67b80a180df9eb050db83445b1f92a4b35bb2
Alan Shmyha <alexey.shmyga@gmail.com>
2022-07-30 14:13:22 +0300

    Adding new settings: - Adding new properties to application.properties, - Changing CommentRepository to paging and sorting type, - Adding HAL explorer

--- src/main/java/travel/goodpoint/server/data/CommentRepository.java
+++ src/main/java/travel/goodpoint/server/data/CommentRepository.java
@@ -1,10 +1,10 @@
 package travel.goodpoint.server.data;
 
 import com.google.common.collect.ImmutableList;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import travel.goodpoint.server.models.Comment;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
 
     ImmutableList<Comment> findByPointId(Long pointId);
 }
